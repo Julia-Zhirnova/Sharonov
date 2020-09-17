d84688238a0acad44c6432f6baf19f93538b1223
Julia-Zhirnova <52165649+Julia-Zhirnova@users.noreply.github.com>
2020-09-17 13:41:12 +0300

    Add files via upload

--- j/main.py
+++ j/main.py
@@ -48,7 +48,38 @@ def calc_ellipse(C):
     a2 = eig[0][1] ** 0.5
     phi1 = eig[1][0][:2]
     phi2 = eig[1][1][:2]
+    print('полуоси')
+    print (a1, a2)
+    print('вектора')
+    print (phi1, phi2)
+    alpha = abs(arctan(phi1[1]/phi1[0]))
+    t = np.linspace(0., 2 * np.pi, 100)                         # изменение параметра t от 0 до 2pi
+    ellipse_matrix = np.array([a1 * np.cos(t), a2 * np.sin(t)]) # параметрическое ур-е эллипса
 
+    rotate_matrix = np.array([[cos(alpha), -sin(alpha)],  # матрица поворота против часовой стрелке
+                              [sin(alpha), cos(alpha)]])
+
+    inv_matrix = np.array([[-1, 0],                              # матрица инверсии по оси y x
+                          [0, -1]])
+    S = inv_matrix.dot(rotate_matrix)                           # итоговоя матрица перехода к базису
+    ellipse_rotated = np.zeros((2, ellipse_matrix.shape[1]))
+
+    for i in range(ellipse_rotated.shape[1]):                   # поворачиваем эллипс
+        ellipse_rotated[:, i] = np.dot(S, ellipse_matrix[:, i]) 
+
+    return ellipse_rotated
+
+def calc_ellipse_rmnk(P_prev):
+
+    eig = np.linalg.eig(P_prev)
+    a1 = eig[0][0] ** 0.5
+    a2 = eig[0][1] ** 0.5
+    phi1 = eig[1][0][:2]
+    phi2 = eig[1][1][:2]
+    print('полуоси')
+    print (a1, a2)
+    print('вектора')
+    print (phi1, phi2)
     alpha = abs(arctan(phi1[1]/phi1[0]))
     t = np.linspace(0., 2 * np.pi, 100)                         # изменение параметра t от 0 до 2pi
     ellipse_matrix = np.array([a1 * np.cos(t), a2 * np.sin(t)]) # параметрическое ур-е эллипса
@@ -76,15 +107,16 @@ def calc_theta_rls(C, m, n):
     for i in range(m):
         z = calc_zhat(C, n)
         S = K + C.dot(P_prev).dot(C.T)
-        P = P_prev - P_prev.dot(C.T).dot(inv(S)).dot(C).dot(P_prev)
+        
         theta_rls = theta_rls_prev + P_prev.dot(C.T).dot(inv(S)).dot(z - C.dot(theta_rls_prev))
+        P = P_prev - P_prev.dot(C.T).dot(inv(S)).dot(C).dot(P_prev)
 
         res[0].append(theta_rls_prev[0])
         res[1].append(theta_rls_prev[1])
 
         theta_rls_prev = theta_rls
         P_prev = P
-
+    print(P)
     return res
 
 def main():
@@ -118,15 +150,23 @@ def main():
     
     ellipse = calc_ellipse(C)
     draw_func(ellipse[0, :], ellipse[1, :], 'mnk')
+	
 
 
-
-    theta_rls = calc_theta_rls(C, 10, n)
-    x = np.array([i for i in range(10)])
+    theta_rls = calc_theta_rls(C, 1000, n)
+    x = np.array([i for i in range(1000)])
     draw_func(x, theta_rls[0], 'Тета 1')
     draw_func(x, theta_rls[1], 'Тета 3')
     print (theta_rls[0])
     print (theta_rls[1])
+    
+    P = np.eye(2) * 0.01
+    ellipse = calc_ellipse_rmnk(P)
+    draw_func(ellipse[0, :], ellipse[1, :], 'rmnk1')
+
+    P = [[1.43071281*0.000000001, 3.36050531*0.00000001], [3.36050531*0.00000001, 1.16825955*0.000001]]
+    ellipse = calc_ellipse_rmnk(P)
+    draw_func(ellipse[0, :], ellipse[1, :], 'rmnk1000')
 
 if __name__ == '__main__':
     main()
